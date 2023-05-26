93298844712dea09ef2964e0226b88aad3f470b8
DaryllCulas <culasdaryll99@gmail.com>
2023-05-27 02:16:54 +0800

    Deleted api.js dbConfig.js dbOperations.js

--- api.js
+++
@@ -1,7 +0,0 @@
-const { reset } = require('nodemon');
-const dboperations = require('./dbOperations');
-//var Db=require('./dbOperations');
-
-dboperations.getLoginDetails().then(result => {
- console.log(result);
-})
