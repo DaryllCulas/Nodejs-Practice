58e2b72c90c5515474c6ba0a11d72d6b1e17753b
DaryllCulas <culasdaryll99@gmail.com>
2023-05-29 02:19:16 +0800

    Modified: server.js && install npm mssql modules

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const sql = require("mssql/msnodesqlv8");
+const mssql = require("mssql");
 
 const app = express();
 app.use("/assets", express.static("assets"));
@@ -7,18 +7,31 @@ app.use("/assets", express.static("assets"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const config = {
-  user: "sa",
-  password: "M@kar0v99",
-  server: "localhost",
-  database: "nodejs",
-  driver: "msnodesqlv8",
-  options: {
-    trustedConnection: true,
-  },
-};
+app.get("/", async function (req, res) {
+  const config = {
+    user: "sa",
+    password: "M@kar0v99",
+    server: "localhost",
+    database: "nodejs",
+    port: 1433,
+    options: {
+      trustServerCertificate: true,
+    },
+  };
 
+  try {
+    await mssql.connect(config);
+    const request = new mssql.Request();
+    const result = await request.query("SELECT * FROM studentTable");
+    res.send(result.recordset);
+  } catch (err) {
+    console.log("Database connection error:", err);
+    res.status(500).send("Database connection error");
+  } finally {
+    mssql.close();
+  }
+});
 
-app.listen(2000, function () {
-  console.log("Server is running on port 2000");
+app.listen(5000, function () {
+  console.log("Server is running on port 5000...");
 });
