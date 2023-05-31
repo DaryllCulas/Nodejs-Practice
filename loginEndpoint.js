240e25bc2e69bcca820d6c65ce9c33945e1e82a1
DaryllCulas <culasdaryll99@gmail.com>
2023-05-31 22:25:03 +0800

    Modified: loginEndpoint.js

--- loginEndpoint.js
+++ loginEndpoint.js
@@ -3,31 +3,65 @@ const mssql = require('mssql');
 
 const router = express.Router();
 
-// Configuration for MSSQL connection
-const config = {
-  user: "sa",
-  password: "M@kar0v99",
-  server: "localhost",
-  database: "nodejs",
-  port: 1433,
-  options: {
-    trustServerCertificate: true,
-  },
-};
 
-// Login endpoint
+// // Configuration for MSSQL connection
+// const config = {
+//   user: "sa",
+//   password: "M@kar0v99",
+//   server: "localhost",
+//   database: "nodejs",
+//   port: 1433,
+//   options: {
+//     trustServerCertificate: true,
+//   },
+// };
+
+// // Login endpoint
+// router.post('/login', (req, res) => {
+//   const { studentID, Email, StudentPassword } = req.body;
+
+//   const pool = new mssql.ConnectionPool(config);
+//   pool.connect((err) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(500).send('Server error');
+//     }
+
+//     const request = new mssql.Request(pool);
+
+//     // const query = `SELECT * FROM studentTable WHERE StudentID = '${studentID}' AND Email = '${Email}' AND StudentPassword = '${StudentPassword}'`;
+    
+
+//     request.query(query, (err, recordset) => {
+//       if (err) {
+//         console.log(err);
+//         return res.status(500).send('Server error');
+//       }
+
+//       if (recordset.recordset.length > 0) {
+//         return res.send('Login successful');
+//       } else {
+//         return res.send('Login failed');
+//       }
+//     });
+//   });
+// });
+
+
+/// Login endpoint
 router.post('/login', (req, res) => {
-  const { studentId, studentEmail, studentPassword } = req.body;
+  const { studentID, Email, StudentPassword } = req.body;
 
-  mssql.connect(config, (err) => {
+  const pool = new mssql.ConnectionPool(config);
+  pool.connect((err) => {
     if (err) {
       console.log(err);
       return res.status(500).send('Server error');
     }
 
-    const request = new mssql.Request();
+    const request = new mssql.Request(pool);
 
-    const query = `SELECT * FROM studentTable WHERE StudentId = '${studentId}' AND StudentEmail = '${studentEmail}' AND StudentPassword = '${studentPassword}'`;
+    const query = `SELECT * FROM studentTable WHERE studentID = '${studentID}' AND Email = '${Email}' AND StudentPassword = '${StudentPassword}'`;
 
     request.query(query, (err, recordset) => {
       if (err) {
@@ -36,12 +70,13 @@ router.post('/login', (req, res) => {
       }
 
       if (recordset.recordset.length > 0) {
-        return res.send('Login successful');
+        res.send('Login successful');
       } else {
-        return res.send('Login failed');
+        res.status(401).send('Login failed');
       }
     });
   });
 });
 
-module.exports = router;
+
+ module.exports = router;
