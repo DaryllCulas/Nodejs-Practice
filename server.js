50221b188a4fb09d1a36f6e564ee9c1100d1796c
DaryllCulas <culasdaryll99@gmail.com>
2023-06-01 00:35:55 +0800

    Modified: server.js > loginEndpoint fixed but should be separated into another file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mssql = require("mssql");
-
+const router = express.Router();
 const app = express();
 const port = 5000;
 
@@ -11,49 +11,65 @@ app.use("/assets", express.static("assets"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Import the login endpoint router
- const loginEndpoint = require('./loginEndpoint');
-
-// Use the login endpoint router
- app.use('/', loginEndpoint);
-
 // Server Config
-app.get("/", async function (req, res) {
-  const config = {
-    user: "sa",
-    password: "M@kar0v99",
-    server: "localhost",
-    database: "nodejs",
-    port: 1433,
-    options: {
-      trustServerCertificate: true,
-    },
-  };
+const config = {
+  user: "sa",
+  password: "M@kar0v99",
+  server: "localhost",
+  database: "nodejs",
+  port: 1433,
+  options: {
+    trustServerCertificate: true,
+  },
+};
 
-  try {
-    // Establish a connection to the SQL server database
-    await mssql.connect(config);
-
-    // Create a new request object
-    const request = new mssql.Request();
-
-    // Execute the query and store the result
-    const result = await request.query("SELECT * FROM studentTable");
-
-    // Send the recordset as the response 
-    res.send(result.recordset);
-  }catch(err) {
-    // Handle database connection or query errors
-    console.log("Database connection error: ", err);
-    res.status(500).send("Database connection error");
-  }finally {
-    // Close the database connection
-    mssql.close();
-  }
-   // Redirect to loginForm
-   res.sendFile(__dirname + "/loginForm.html");
+// Redirect to loginForm
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/loginForm.html");
 });
+
+// Login endpoint
+router.post('/login', (req, res) => {
+  const { studentID, Email, StudentPassword } = req.body;
+
+  const pool = new mssql.ConnectionPool(config);
+  pool.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Server error');
+    }
+
+    const request = new mssql.Request(pool);
+
+    const query = `SELECT * FROM studentTable WHERE studentID = '${studentID}' AND Email = '${Email}' AND StudentPassword = '${StudentPassword}'`;
+
+    request.query(query, (err, recordset) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Server error');
+      }
+
+      if (recordset.recordset.length > 0) {
+        // // res.send('Login successful');
+         return res.redirect("/dashboard"); // Redirect to the dashboard page
+        // res.json({ success: true, message: 'Login successful' });
+        // return res.redirect("/dashboard"); // Redirect to the dashboard page
+      } else {
+         return res.status(401).send('Login failed');
+        // res.status(401).json({ success: false, message: 'Login failed' });
+      }
+    });
+  });
+});
+
+app.get("/dashboard", function (req, res) {
+  res.sendFile(__dirname + "/dashboard.html");
+});
+
+app.use("/", router);
 
 app.listen(port, function () {
   console.log(`Server is running on ${port}`);
 });
+
+module.exports = router;
