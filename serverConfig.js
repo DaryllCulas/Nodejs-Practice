abe2e025b412859c00dbf44ed6c6e8e7610831c0
DaryllCulas <culasdaryll99@gmail.com>
2023-06-05 01:20:23 +0800

    Add serverConfig from server.js (updated)

---
+++ serverConfig.js
@@ -0,0 +1,13 @@
+// Server Config
+const config = {
+  user: "sa",
+  password: "M@kar0v99",
+  server: "localhost",
+  database: "nodejs",
+  port: 1433,
+  options: {
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = config;
